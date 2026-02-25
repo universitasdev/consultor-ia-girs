ab35075ae028c8941b90d4bd218f6cf7b3ec8e34
joseA <nobujose6@gmail.com>
2026-02-25 14:59:58 -0400

    fix: Escuchar puerto dinamico para Cloud Run

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap().catch((err) => {
